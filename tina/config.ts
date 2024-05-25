17207ec8a2ec5cd5ef2509f49410050787a7e9cf
Viola Kosíková <viola.kosikova@morosystems.cz>
2024-05-25 20:07:57 +0200

    Build without astro check

--- tina/config.ts
+++ tina/config.ts
@@ -31,6 +31,7 @@ export default defineConfig({
         name: "post",
         label: "Posts",
         path: "content/posts",
+        format: "mdx",
         fields: [
           {
             type: "string",
